b0642d24ebbbcf789d33217a848eb4f7b067370d
Joonas <joonas.yoon@gmail.com>
2018-01-10 03:25:14 +0000

    서비스에서 로그 찍히게 변경

--- service.sh
+++ service.sh
@@ -16,5 +16,5 @@ echo "==================================" >> $LOGFILE
 echo "Start on [ $CUR_DATE ]" >> $LOGFILE
 
 # https://askubuntu.com/questions/420981/how-do-i-save-terminal-output-to-a-file
-dmoj -c $HOME/config.yml 0.0.0.0 &>> $LOGFILE &
+dmoj -c $HOME/config.yml 0.0.0.0 >> $LOGFILE
 
